a90d620ab2698ef0a5d042b85b973c3a9b58e2aa
Korey Ray <phentos@gmail.com>
2024-04-02 13:44:54 -0700

    improved highlight logic

--- outcomeTool.js
+++ outcomeTool.js
@@ -3,196 +3,207 @@ let t;
 let dragged;
 
 function parseAndDisplay() {
-	populateTable(fetchParse());
+  populateTable(fetchParse());
 }
 
 function populateTable(entries) {
-	t = document.getElementById("results");
-	activateHeaderDrops();
+  t = document.getElementById("results");
+  activateHeaderDrops();
 
-	while (entries.length) {
-		let ne = entries.pop();
-		let ol = getOutcomes(ne);
-		let tb = "electives";
+  while (entries.length) {
+    let ne = entries.pop();
+    let ol = getOutcomes(ne);
 
-		for (o of ol) {
-			let ttb = document.getElementById(`${o}Classes`);
+    for (o of ol) {
+      let cap = o == "electives" ? 6 : 3;
 
-			if (o == "uncounted" ? true : ttb.childElementCount < 3) {
-				tb = o;
-				break;
-			}
-		}
-
-		addEntryToTable(ne, tb);
-	}
-	updateCounts();
+      if (o == "uncounted" ? true : getcc(o) < cap) {
+        let otb = document.getElementById(`${o}Classes`);
+        addEntryToTable(ne, otb);
+        break;
+      }
+    }
+  }
+  updateCounts();
 }
 
+/**
+ *
+ * @param {string} ne	a class code, e.g. 'CSE 100'
+ * @param {string} tb	the table to add to
+ */
 function addEntryToTable(ne, tb) {
-	let tr = conjureRow(ne);
-	document.getElementById(`${tb}Classes`).appendChild(tr);
+  tb.appendChild(makeCell(ne));
 }
 
-function conjureRow(e) {
-	let td = document.createElement("td");
+function makeCell(e) {
+  let td = document.createElement("td");
+  td.textContent = e;
 
-	td.textContent = e;
-	let ol = getOutcomes(e);
+  let ol = getOutcomes(e);
+  for (o of ol) {
+    td.classList.add(o);
+  }
 
-	for (o of ol) {
-		td.classList.add(o);
-	}
+  if (!(ol.includes("core") || ol.includes("uncounted"))) {
+    td.classList.add("electives");
+  }
 
-	if (!ol.includes("core") && !ol.includes("uncounted")) {
-		td.classList.add("electives");
-	}
+  if (!td.classList.contains("core")) {
+    activateDrag(td);
+  }
 
-	bindEvents(td);
-
-	return td;
+  return td;
 }
 
-function bindEvents(td) {
-	if (td.classList.contains("core")) {
-		return;
-	}
-	td.setAttribute("draggable", "true");
-	td.addEventListener("dragstart", (event) => {
-		dragged = event.target;
-		toggleHighlight(event.target.classList, true);
-		event.target.classList.add("dragging");
-	});
+function activateDrag(td) {
+  const tdo = function (e) {
+    return getOutcomes(e.target.textContent);
+  };
+  td.setAttribute("draggable", "true");
+  td.addEventListener("dragstart", (event) => {
+    dragged = event.target;
+    toggleHighlight(tdo(event), true);
+    event.target.classList.add("dragging");
+  });
 
-	td.addEventListener("dragend", (event) => {
-		event.target.classList.remove("dragging");
-		toggleHighlight(event.target.classList, false);
-	});
+  td.addEventListener("dragend", (event) => {
+    event.target.classList.remove("dragging");
+    toggleHighlight(tdo(event), false);
+  });
 
-	td.addEventListener("mouseenter", (event) => {
-		toggleHighlight(event.target.classList, true);
-	});
+  td.addEventListener("mouseenter", (event) => {
+    toggleHighlight(tdo(event), true);
+  });
 
-	td.addEventListener("mouseleave", (event) => {
-		toggleHighlight(event.target.classList, false);
-	});
+  td.addEventListener("mouseleave", (event) => {
+    toggleHighlight(tdo(event), false);
+  });
 }
 
-function toggleHighlight(cl, add) {
-	for (c of [...cl, "electives"]) {
-		add
-			? outcomeHeaders[c].classList.add("highlight")
-			: outcomeHeaders[c].classList.remove("highlight");
-	}
+let debug = true;
+function toggleHighlight(ol, add) {
+  for (o of ol) {
+    add
+      ? outcomeHeaders[o].classList.add("highlight")
+      : outcomeHeaders[o].classList.remove("highlight");
+  }
 }
 
 function bindDrops(th) {
-	th.addEventListener("dragover", (event) => {
-		if (dragged.classList.contains(event.target.classList[0])) {
-			event.preventDefault();
-		}
-	});
+  th.addEventListener("dragover", (event) => {
+    if (dragged.classList.contains(event.target.classList[0])) {
+      event.preventDefault();
+    }
+  });
 
-	th.addEventListener("dragenter", (event) => {
-		if (dragged.classList.contains(event.target.classList[0])) {
-			event.target.classList.add("dragover");
-		}
-	});
+  th.addEventListener("dragenter", (event) => {
+    if (dragged.classList.contains(event.target.classList[0])) {
+      event.target.classList.add("dragover");
+    }
+  });
 
-	th.addEventListener("dragleave", (event) => {
-		if (event.target.classList.contains("dragover")) {
-			event.target.classList.remove("dragover");
-		}
-	});
+  th.addEventListener("dragleave", (event) => {
+    if (event.target.classList.contains("dragover")) {
+      event.target.classList.remove("dragover");
+    }
+  });
 
-	th.addEventListener("drop", (event) => {
-		event.preventDefault();
-		event.target.classList.remove("dragover");
-		moveCell(event.target);
-	});
+  th.addEventListener("drop", (event) => {
+    event.preventDefault();
+    event.target.classList.remove("dragover");
+    moveCell(event.target);
+  });
 }
 
 function moveCell(where) {
-	let o = where.classList[0];
+  let o = where.classList[0];
 
-	dragged.parentNode.removeChild(dragged);
-	document.getElementById(`${o}Classes`).appendChild(dragged);
-	dragged = null;
+  dragged.parentNode.removeChild(dragged);
+  document.getElementById(`${o}Classes`).appendChild(dragged);
+  dragged = null;
 
-	updateCounts();
+  updateCounts();
 }
 
 const outcomeHeaders = {};
 function activateHeaderDrops() {
-	t.querySelectorAll("th").forEach((th) => {
-		if (!th.classList.contains("core")) {
-			bindDrops(th);
-		}
-		outcomeHeaders[th.classList[0]] = th;
-	});
+  t.querySelectorAll("th").forEach((th) => {
+    if (!th.classList.contains("core")) {
+      bindDrops(th);
+    }
+    outcomeHeaders[th.classList[0]] = th;
+  });
 }
 
 function updateCounts() {
-	for (o of ["core", "systems", "theory", "applications"]) {
-		setNeeds(o, 3);
-	}
+  for (o of ["core", "systems", "theory", "applications"]) {
+    setNeeds(o, 3);
+  }
 
-	setNeeds("electives", 6);
+  setNeeds("electives", 6);
 }
 
 function setNeeds(o, req) {
-	let cc = getcc(o);
-	let r = cc >= req ? 0 : req - cc;
-	let po = o[0].toUpperCase() + o.slice(1);
-	outcomeHeaders[o].innerText = `${po} (${r} needed)`;
+  let cc = getcc(o);
+  let r = cc >= req ? 0 : req - cc;
+  let po = o[0].toUpperCase() + o.slice(1);
+  outcomeHeaders[o].innerText = `${po} (${r} needed)`;
 }
 
 function getcc(o) {
-	return document.getElementById(`${o}Classes`).childElementCount;
+  return document.getElementById(`${o}Classes`).childElementCount;
 }
 
 function getClasses(o) {
-	let cl = conversions["outcomeToClasses"][o];
-	return cl ? cl : console.warn(`getClasses called on bad outcome ${o}`);
+  let cl = conversions["outcomeToClasses"][o];
+  return cl ? cl : console.warn(`getClasses called on bad outcome ${o}`);
 }
 
 function getOutcomes(c) {
-	let ol = conversions["classToOutcomes"][c];
-	return ol ? ol : ["uncounted"];
+  let ol = conversions["classToOutcomes"][c];
+  if (ol) {
+    if (!(ol.includes("core") || ol.includes("electives"))) {
+      ol.push("electives");
+    }
+  } else {
+    ol = ["uncounted"];
+  }
+  return ol;
 }
 
 function fetchParse() {
-	const checkbox = document.getElementById("auditCheckbox");
-	let input = document.getElementById("classInput").value;
+  const checkbox = document.getElementById("auditCheckbox");
+  let input = document.getElementById("classInput").value;
 
-	if (checkbox.checked) {
-		input = parseAudit(input);
-	}
+  if (checkbox.checked) {
+    input = parseAudit(input);
+  }
 
-	return input.split(",").map((s) => s.replaceAll(" ", "").toUpperCase());
+  return input.split(",").map((s) => s.replaceAll(" ", "").toUpperCase());
 }
 
 function parseAudit(input) {
-	const auditLines = input.replaceAll(" ", "").split("\n");
-	const classList = [];
-	auditLines.forEach((line) => {
-		const parts = line.split("\t");
-		if (parts.length > 1 && parts[1].trim() !== "") {
-			classList.push(parts[1].trim());
-		}
-	});
-	return classList.join(",");
+  const auditLines = input.replaceAll(" ", "").split("\n");
+  const classList = [];
+  auditLines.forEach((line) => {
+    const parts = line.split("\t");
+    if (parts.length > 1 && parts[1].trim() !== "") {
+      classList.push(parts[1].trim());
+    }
+  });
+  return classList.join(",");
 }
 
 function toggleAuditTemplate() {
-	const checkbox = document.getElementById("auditCheckbox");
-	const textarea = document.getElementById("classInput");
-	const auditText = `FA23\tCOGS108\t4.00\tA+\t\t\nData Science in Practice\nSP22\tEDS 124BR\t4.00\tB+\t\t\nTeach Computational Thinking\nSP22\tLIGN165\t4.00\tB+\t\t\nComputational Linguistics`;
-	textarea.value = "";
+  const checkbox = document.getElementById("auditCheckbox");
+  const textarea = document.getElementById("classInput");
+  const auditText = `FA23\tCOGS108\t4.00\tA+\t\t\nData Science in Practice\nSP22\tEDS 124BR\t4.00\tB+\t\t\nTeach Computational Thinking\nSP22\tLIGN165\t4.00\tB+\t\t\nComputational Linguistics`;
+  textarea.value = "";
 
-	if (checkbox.checked) {
-		textarea.placeholder = auditText;
-	} else {
-		textarea.placeholder = "CSE 100, CSE 101, CSE 110";
-	}
+  if (checkbox.checked) {
+    textarea.placeholder = auditText;
+  } else {
+    textarea.placeholder = "CSE 100, CSE 101, CSE 110";
+  }
 }
